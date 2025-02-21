9acb827ec9dcccc40e6a2131ee0777527aaf6ec0
gebriel <161641473+gebriel-admasu@users.noreply.github.com>
2025-02-21 23:05:36 +0300

    Time: 43 ms (89.09%) | Memory: 18.2 MB (35.91%) - LeetSync

--- 3195-separate-black-and-white-balls/separate-black-and-white-balls.py
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.py
@@ -1,12 +1,12 @@
 class Solution:
     def minimumSteps(self, s: str) -> int:
         count = 0
-        zero_count = 0  # Tracks number of '0's seen
+        zero_count = 0  
         
         for char in s:
             if char == '0':
-                count += zero_count  # Each zero contributes swaps
+                count += zero_count 
             else:
-                zero_count += 1  # Counting '1's encountered
+                zero_count += 1  
         
         return count
