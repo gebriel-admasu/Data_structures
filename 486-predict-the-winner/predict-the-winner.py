53cd6e8010c36c76d6c7c5e331ec875e967c3b77
gebriel <161641473+gebriel-admasu@users.noreply.github.com>
2025-03-13 17:04:25 +0300

    Time: 2430 ms (13.01%) | Memory: 17.7 MB (90.90%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.py
+++ 486-predict-the-winner/predict-the-winner.py
@@ -1,12 +1,14 @@
-from typing import List
-
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
-        def helper(start, end):
+        def recurse(start, end):
             if start == end:
                 return nums[start]
-            pick_start = nums[start] - helper(start + 1, end)
-            pick_end = nums[end] - helper(start, end - 1)
+            pick_start = nums[start] - recurse(start + 1, end)
+            pick_end = nums[end] - recurse(start, end - 1)
             return max(pick_start, pick_end)
+        res = recurse(0, len(nums) - 1)
+        if res >=0:
 
-        return helper(0, len(nums) - 1) >= 0
+            return  True
+        else:
+            return False
