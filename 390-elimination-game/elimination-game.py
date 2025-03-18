b500a857da238f513e0ce075207ce977d4b9eb3d
gebriel <161641473+gebriel-admasu@users.noreply.github.com>
2025-03-18 18:19:01 +0300

    Time: 7 ms (21.92%) | Memory: 17.6 MB (92.49%) - LeetSync

--- 390-elimination-game/elimination-game.py
+++ 390-elimination-game/elimination-game.py
@@ -1,27 +1,5 @@
 class Solution:
     def lastRemaining(self, n: int) -> int:
-        last_element,first_element = n,1
-
-        iteration,step,remaining_count = 0,1,n
-
-        while remaining_count > 1:
-            if iteration %  2 != 0:
-                last_element -= step
-
-                if remaining_count %2:
-                    first_element +=step
-            else:
-                first_element +=step
-                if remaining_count %2:
-                    last_element -= step
-            remaining_count >>=1
-            step <<= 1
-
-
-
-            iteration +=1
-        return first_element
-            
-                
-
-        
+        if n == 1:
+            return 1
+        return 2 * (1 + (n // 2 - self.lastRemaining(n // 2)))
